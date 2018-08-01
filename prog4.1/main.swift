268302871868f17328af6f114ef1acc8cfe9f783
Hugh McLeod-Jones <hmcleodjones@oucs1432.otago.ac.nz>
2018-08-01 12:51:53 +1200

    Initial Commit

---
+++ prog4.1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  prog4.1
+//
+//  Created by Hugh McLeod-Jones on 8/1/18.
+//  Copyright © 2018 Hugh McLeod-Jones. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
